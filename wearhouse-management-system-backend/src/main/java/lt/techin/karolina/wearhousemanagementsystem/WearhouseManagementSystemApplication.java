9fe614b878c86394db9424d4a38d33d93a9ad620
Karolina Čižaitė <61018680+kcizaite@users.noreply.github.com>
2023-02-27 17:01:37 -0800

    Initial commit

---
+++ wearhouse-management-system-backend/src/main/java/lt/techin/karolina/wearhousemanagementsystem/WearhouseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.karolina.wearhousemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearhouseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearhouseManagementSystemApplication.class, args);
+	}
+
+}
